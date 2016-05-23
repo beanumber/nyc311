23f12be3ff695154de37d63f50bfac01c172e1ce
liwencong1995 <liwencong1995@gmail.com>
2016-05-23 16:13:50 -0400

    fixed etl_extract

--- R/etl_extract.R
+++ R/etl_extract.R
@@ -1,19 +1,34 @@
+#' using 311 data
+#' 
+#' @export
+#' @importFrom RSocrata read.socrata
+#' 
+#' @examples 
+#' 
+#' calls <- etl("nyc311")
+#' calls %>%
+#'   etl_extract()
+#'
+
 
 #etl extract
 etl_extract.etl_nyc311 <- function(obj, year = 2015, month = 03, day =18, ...) {
-  years <- intersect(1987:2015, years)
-  months <- intersect(01:12, months)
-  days <- intersect(01:30, days)
+  #this_year <- as.numeric(format(Sys.Date(), '%Y'))
+  #years <- intersect(1987:this_year, years)
+  #months <- intersect(01:12, months)
+  #days <- intersect(01:30, days)
   
   base_url <- "https://data.cityofnewyork.us/resource/"
   date = paste0( "'", year, "-", month, "-", day,  "'")
   src <- paste0(base_url, "fhrw-4uyv.csv?$where=date_trunc_ymd(created_date)=", date)
   dir <- attr(obj, "raw_dir")
   lcl <- paste0(dir, "/", basename(src))
+  #read.csv(url(src))
   # I don't know how to download this data. 
   # download.file does not work for this data set
-  #? download.file(src, lcl, method = 'curl')
+  download.file(src, "~/Desktop/", method = 'curl')
 }
+
 
 #etl transorm
 etl_transform.etl_nyc311 <- function(obj,  year = 2015, month = 03, day =18, ...) {
@@ -21,7 +36,7 @@ etl_transform.etl_nyc311 <- function(obj,  year = 2015, month = 03, day =18, ...
   base_url <- "https://data.cityofnewyork.us/resource/"
   src <- paste0(base_url, "fhrw-4uyv.csv?$where=date_trunc_ymd(created_date)=", date)
   new_dir <- attr(obj, "load_dir")
-  read.csv(url(src))
+  #read.csv(url(src))
   #I donno how to specify a dir for this csv file
 }
 
---
+++ data-raw/Priscilla.R
@@ -0,0 +1,31 @@
+
+
+#example
+year<-2015
+month<-03
+day<-18
+
+require(rvest)
+nyc311<- read_html("https://data.cityofnewyork.us/Social-Services/311-Service-Requests-from-2010-to-Present/erm2-nwe9")
+class(nyc311)
+nyc311_context <- nyc311%>%
+  html_nodes(".blist-td blist-t1-c217215390 blist-td-date") %>%
+  html_text()
+
+
+#endpoint
+#https://data.cityofnewyork.us/OData.svc/erm2-nwe9
+
+#?$where=date_trunc_ymd(created_date)='2015-3-18'
+
+#https://data.cityofnewyork.us/resource/fhrw-4uyv.json?$where=date_trunc_ymd(created_date)='2015-3-18'
+
+
+#
+#https://data.ny.gov/api/views/fhrw-4uyv?$where=date_trunc_ymd(created_date)='2015-3-18'
+
+# zip and unzip 
+#test <-read.csv(url(src))
+#test_zip<-export(test, "test.csv.zip",format = "csv")
+#unzip(test_zip, exdir = "~/Desktop/")
+
