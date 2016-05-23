4360652e49dde314f737e7eeeb185c7aef3c416b
liwencong1995 <liwencong1995@gmail.com>
2016-05-23 16:29:39 -0400

    Added etl_transform

--- R/etl_extract.R
+++ R/etl_extract.R
@@ -7,11 +7,9 @@
 #' 
 #' calls <- etl("nyc311")
 #' calls %>%
-#'   etl_extract()
+#'   etl_extract() %>%
+#'   etl_transform()
 #'
-
-
-#etl extract
 etl_extract.etl_nyc311 <- function(obj, year = 2015, month = 03, day =18, ...) {
   #this_year <- as.numeric(format(Sys.Date(), '%Y'))
   #years <- intersect(1987:this_year, years)
@@ -23,22 +21,17 @@ etl_extract.etl_nyc311 <- function(obj, year = 2015, month = 03, day =18, ...) {
   src <- paste0(base_url, "fhrw-4uyv.csv?$where=date_trunc_ymd(created_date)=", date)
   dir <- attr(obj, "raw_dir")
   lcl <- paste0(dir, "/", basename(src))
-  #read.csv(url(src))
-  # I don't know how to download this data. 
-  # download.file does not work for this data set
   download.file(src, lcl, method = 'curl')
   invisible(obj)
 }
 
-
-#etl transorm
+#' @export
+#' @rdname etl_extract.etl_nyc311
 etl_transform.etl_nyc311 <- function(obj,  year = 2015, month = 03, day =18, ...) {
   dir <- attr(obj, "raw_dir")
-  base_url <- "https://data.cityofnewyork.us/resource/"
-  src <- paste0(base_url, "fhrw-4uyv.csv?$where=date_trunc_ymd(created_date)=", date)
   new_dir <- attr(obj, "load_dir")
-  #read.csv(url(src))
-  #I donno how to specify a dir for this csv file
+  file.copy(dir, new_dir)
+  invisible(obj)
 }
 
 #etl load
