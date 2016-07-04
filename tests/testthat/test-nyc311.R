d89b528c1530086f2b870977803ec2a1f4a55105
liwencong1995 <liwencong1995@gmail.com>
2016-07-04 16:29:11 -0400

    Updated test.#11

--- tests/testthat/test-nyc311.R
+++ tests/testthat/test-nyc311.R
@@ -6,6 +6,22 @@ context("nyc311")
 test_that("download works", {
   url <- "http://data.cityofnewyork.us/resource/fhrw-4uyv.csv?$where=created_date%20between%20'2016-06-06'%20and%20'2016-06-07'&$limit=10"
   lcl <- tempfile()
-#  options()$download.file.method
-  expect_equal(download.file(url, lcl), 0)
+  
+  download <-
+    {
+      first_try <- tryCatch(
+        utils::download.file(url, lcl,method = "curl"),
+        error = function(e){warning(e)},
+        finally = 'method = "curl" fails')
+      
+      ifelse(class(first_try) == "NULL", 
+             print("Download succeeded."), 
+             tryCatch(utils::download.file(url, lcl),
+                      error = function(e){warning(e)},
+                      finally = 'method = "auto" fails'))
+      }
+  expect_equal(download, 0)
 })
+
+
+
