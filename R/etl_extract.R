2646df9ac3fac0712ebd25616041c3f562f85485
liwencong1995 <liwencong1995@gmail.com>
2016-07-07 18:21:21 -0400

    renamed n #12

--- R/etl_extract.R
+++ R/etl_extract.R
@@ -9,12 +9,12 @@
 #' @inheritParams etl::etl_extract
 #' @param years years that the issues have been created (the most recent year is the default)
 #' @param months months that the issues have been created (the most recent month is the default)
-#' @param n number of readings (1000000 is the default)
+#' @param num_calls number of readings (1000000 is the default)
 #' @param ... arguments passed to \code{\link{download.file}}. Set method as "curl" for Linux system, and as default for Windows and Mac OS X systems.
 #' @details This function downloads NYC311 data from NYC311 OPEN DATA website for years and months specified.
 etl_extract.etl_nyc311 <- function(obj, years = lubridate::year(Sys.Date()), 
                                    months = lubridate::month(Sys.Date()), 
-                                   n = 1000000, ...) {
+                                   num_calls = 1000000, ...) {
   #check if the year is valid
   valid_months <- etl::valid_year_month(years, months, begin = "2010-01-01")
 
@@ -22,7 +22,7 @@ etl_extract.etl_nyc311 <- function(obj, years = lubridate::year(Sys.Date()),
   base_url <- "https://data.cityofnewyork.us/resource/fhrw-4uyv.csv"
   valid_months <- mutate_(valid_months, src = ~paste0(base_url, 
                   "?$where=created_date%20between%20'", month_begin,
-                  "'%20and%20'", month_end, "'&$limit=", format(n, scientific = FALSE)))
+                  "'%20and%20'", month_end, "'&$limit=", format(num_calls, scientific = FALSE)))
   src_length <- nrow(valid_months)
   dir <- attr(obj, "raw_dir")
   valid_months <- mutate(valid_months, lcl = paste0(dir, "/nyc311_", valid_months$year, "_", valid_months$month, ".csv"))
