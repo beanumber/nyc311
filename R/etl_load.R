7e29da360278d74a5a85b8df8d92d161cb86d2db
beanumber <ben.baumer@gmail.com>
2016-07-07 13:38:01 -0600

    updated etl_load and fixed tests

--- R/etl_load.R
+++ R/etl_load.R
@@ -7,7 +7,6 @@
 #' @importFrom DBI dbWriteTable
 #' @importFrom lubridate year month
 #' @inheritParams etl::etl_extract
-#' @param schema schema
 #' @details This function loads NYC311 data into a local database for years and months specified.
 #' @examples 
 #' \dontrun{
@@ -25,7 +24,7 @@
 #'   tbl("calls") %>%
 #'   collect()
 #' }
-etl_load.etl_nyc311 <- function(obj, schema = FALSE, years = lubridate::year(Sys.Date()), 
+etl_load.etl_nyc311 <- function(obj, years = lubridate::year(Sys.Date()), 
                                 months = lubridate::month(Sys.Date()), ...) {
   #check if the year is valid
   valid_months <- etl::valid_year_month(years, months, begin = "2010-01-01")
--- tests/testthat/test-nyc311.R
+++ tests/testthat/test-nyc311.R
@@ -4,29 +4,20 @@ context("nyc311")
 ## TODO: Add more tests
 
 test_that("instantiation works", {
-  ontime <- etl("nyc311") 
-  expect_is(ontime, c("etl_nyc311", "src_sql"))
+  calls <- etl("nyc311") 
+  expect_is(calls, c("etl_nyc311", "src_sql"))
 })
 
 
-test_that("download works", {
-  url <- "http://data.cityofnewyork.us/resource/fhrw-4uyv.csv?$where=created_date%20between%20'2016-06-06'%20and%20'2016-06-07'&$limit=10"
-  lcl <- tempfile()
-  
-  download <-
-    {
-      first_try <- tryCatch(
-        utils::download.file(url, lcl,method = "curl"),
-        error = function(e){warning(e)},
-        finally = 'method = "curl" fails')
-      
-      ifelse(class(first_try) == "NULL", 
-             print("Download succeeded."), 
-             tryCatch(utils::download.file(url, lcl),
-                      error = function(e){warning(e)},
-                      finally = 'method = "auto" fails'))
-      }
-  expect_equal(download, 0)
+test_that("sqlite works", {
+  test_dir <- "~/Desktop/nyc311"
+  if (dir.exists(test_dir)) {
+    expect_s3_class(calls_sqlite <- etl("nyc311", dir = test_dir), "src_sqlite")
+    expect_message(calls_sqlite %>% etl_init(), "Could not find")
+    expect_message(calls_sqlite %>% etl_update(years = 2011, months = 1), "Writing NYC311 data")
+    expect_output(print(calls_sqlite), "calls")
+    expect_equal(calls_sqlite %>% tbl("calls") %>% collect(n = Inf) %>% nrow(), 170556)
+  }
 })
 
 
@@ -38,11 +29,11 @@ test_that("mysql works", {
                     user = NULL, password = NULL)
     test_dir <- "~/Desktop/nyc311"
     if (dir.exists(test_dir)) {
-      expect_s3_class(ontime_mysql <- etl("airlines", db = db, dir = test_dir), "src_mysql")
-      ontime_mysql %>% etl_init()
-      expect_message(ontime_mysql %>% etl_update(years = 2011, months = 1, n = 10), "success")
-      expect_output(print(ontime_mysql), "calls")
-      expect_equal(ontime_mysql %>% tbl("calls") %>% collect(n = Inf) %>% nrow(), 448620)
+      expect_s3_class(calls_mysql <- etl("nyc311", db = db, dir = test_dir), "src_mysql")
+      expect_message(calls_mysql %>% etl_init(), "Could not find")
+      expect_message(calls_mysql %>% etl_update(years = 2011, months = 1), "Writing NYC311 data")
+      expect_output(print(calls_mysql), "calls")
+      expect_equal(calls_mysql %>% tbl("calls") %>% collect(n = Inf) %>% nrow(), 170556)
     }
   }
 })
