dc742f9df4f089061925e7ddbe850be160e85f6f
liwencong1995 <liwencong1995@gmail.com>
2017-02-16 10:39:02 -0500

    Debugging test. "mysql works". #11

--- tests/testthat/test-nyc311.R
+++ tests/testthat/test-nyc311.R
@@ -13,12 +13,13 @@ test_that("sqlite works", {
   if (require(RSQLite)) {
     expect_s3_class(calls_sqlite <- etl("nyc311"), "src_sqlite")
     expect_message(calls_sqlite %>% etl_init(), "Could not find")
-    expect_message(calls_sqlite %>% etl_update(years = 2010:2011, months = 1:3, num_calls = 100), "Writing NYC311 data")
+    expect_message(calls_sqlite %>% 
+                     etl_update(years = 2010:2011, months = 1:3, num_calls = 100), "Writing NYC311 data")
     expect_output(print(calls_sqlite), "calls")
-    expect_equal(calls_sqlite %>% tbl("calls") %>% collect(n = Inf) %>% nrow(), 100)
+    expect_equal(calls_sqlite %>% 
+                   tbl("calls") %>% collect() %>% nrow(), 600)
   }
 })
-
 
 
 test_that("mysql works", {
@@ -28,9 +29,11 @@ test_that("mysql works", {
                     user = NULL, password = NULL)
     expect_s3_class(calls_mysql <- etl("nyc311", db = db, dir = test_dir), "src_mysql")
     expect_message(calls_mysql %>% etl_init(), "Could not find")
-    expect_message(calls_mysql %>% etl_update(years = 2010:2011, months = 1:3, num_calls = 100), "Writing NYC311 data")
+    expect_message(calls_mysql 
+                   %>% etl_update(years = 2010:2011, months = 1:3, num_calls = 100), "Writing NYC311 data")
     expect_output(print(calls_mysql), "calls")
-    expect_equal(calls_mysql %>% tbl("calls") %>% collect(n = Inf) %>% nrow(), 100)
+    expect_equal(calls_mysql 
+                 %>% tbl("calls") %>% collect() %>% nrow(), 600)
   }
 })
 
