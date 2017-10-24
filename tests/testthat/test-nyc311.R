1ec08f2695f42e1204b1425dd8b0f9b0d7347e06
Priscilla Li <priscilla@Priscillas-MacBook-Air.local>
2017-10-24 19:34:05 -0400

    Updated cran-comments.

--- tests/testthat/test-nyc311.R
+++ tests/testthat/test-nyc311.R
@@ -3,3 +3,8 @@ context("nyc311")
 ## TODO: Rename context
 ## TODO: Add more tests
 
+test_that("etl nyctaxi", {
+  calls <- etl("nyc311")
+  expect_s3_class(calls, c("etl_nyctaxi", "etl", "src_sqlite", "src_sql", "src"))
+  expect_message(calls %>% etl_extract(yeears = 2010, months =1, num_calls = 10), "Extracting")
+})
