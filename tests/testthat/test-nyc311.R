4a11a47d45a2dee6d9f0b73e81f8abb4c5c56a04
Priscilla Li <priscilla@Priscillas-MacBook-Air.local>
2017-10-25 12:26:45 -0400

    Fixed typo in vignette.

--- tests/testthat/test-nyc311.R
+++ tests/testthat/test-nyc311.R
@@ -3,7 +3,7 @@ context("nyc311")
 ## TODO: Rename context
 ## TODO: Add more tests
 
-test_that("etl nyctaxi", {
+test_that("etl nyc311", {
   calls <- etl("nyc311")
   expect_s3_class(calls, c("etl_nyctaxi", "etl", "src_sqlite", "src_sql", "src"))
   expect_message(calls %>% etl_extract(yeears = 2010, months =1, num_calls = 10), "Extracting")
